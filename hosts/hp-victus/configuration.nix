05be229a23e4bc4a4286252d98c98fbeb8b1383f
cristhalt <203342643+cristhalt@users.noreply.github.com>
2025-03-29 16:53:12 +0000

    feat: setup nvidia

--- hosts/hp-victus/configuration.nix
+++ hosts/hp-victus/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -10,6 +10,29 @@
 
   boot.initrd.luks.devices."luks-bf221af3-6352-43d5-9b96-40b06238dbf7".device =
     "/dev/disk/by-uuid/bf221af3-6352-43d5-9b96-40b06238dbf7";
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  hardware.nvidia = {
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:5:0:0";
+    };
+  };
+
+  services.xserver.videoDrivers = [
+    "amdgpu"
+    "nvidia"
+  ];
 
   networking.hostName = "hp-victus";
 }
